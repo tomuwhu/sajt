c07de00e8bb452a7406ee4dc04bf608c4e853fbb
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-30 15:00:02 +0200

    .

--- frontend/src/routes/reg/+page.svelte
+++ frontend/src/routes/reg/+page.svelte
@@ -12,14 +12,17 @@
 <h1>Regisztráció</h1>
 <form>
     {#each ml as x}
-    <input type="text" placeholder={x[0]} autocomplete="off" bind:value={user[x[1]]}>
+    <input type="text" placeholder={x[0]} bind:value={user[x[1]]}>
     <br>
     <br>
     {/each}
-    <input placeholder="Jelszó" type="password" autocomplete="new-password" bind:value={pw}>
+    <input placeholder="Jelszó (min. 4 karakter)" type="password" autocomplete="new-password" bind:value={pw}>
     <br><br>
     <button 
-        disabled={!(user.un && user.nev && pw && pw.length > 7 && user.mail)}
+        disabled={!(
+            user.un && user.nev && pw && pw.length > 3 &&
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user.mail)
+        )}
         on:click={() => {
             var tosend = Object.entries(user).map(v => v.join('¨')).join('|')
             fetch(`${serverbase}/reg/${tosend}`)
